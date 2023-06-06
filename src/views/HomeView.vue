7f5a104e1a5aa4d8c3aa4873d0b3e6a13f17801f
Vlad Gheorghiu <vlad.gheorghiu@pitechplus.onmicrosoft.com>
2023-06-06 16:31:09 +0300

    feature: data refs and reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,40 @@
 <template>
   <div class="home">
-    <h1>Counter</h1>
+    <h1>{{ appTitle }}</h1>
+    <br>
+
+    <h4>{{ counterData.title }}:</h4>
     
     <div>
       <button @click="decreaseCounter()" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter()" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
     </div>
 
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue';
+  import { ref, reactive } from 'vue';
 
-  const counter = ref(0);
+const appTitle = 'My Amazing Counter Title'
+
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter'
+  });
 
   const increaseCounter = () => {
-    counter.value++;
+    counterData.count++;
   }
 
   const decreaseCounter = () => {
-    counter.value--;
+    counterData.count--;
   }
 </script>
 
@@ -33,6 +46,11 @@
 
   .btn, .counter {
     font-size: 40px;
-    margin: 10px;  
+    margin: 10px;
+    width: 64px;  
+  }
+
+  .edit {
+    margin-top: 60px;
   }
 </style>
