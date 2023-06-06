aae035f35920ce28ec1d307348ab909c64ad2a74
Vlad Gheorghiu <vlad.gheorghiu@pitechplus.onmicrosoft.com>
2023-06-06 15:49:27 +0300

    project setup and basics

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Counter</h1>
+    
+    <div>
+      <button @click="decreaseCounter()" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter()" class="btn">+</button>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const counter = ref(0);
+
+  const increaseCounter = () => {
+    counter.value++;
+  }
+
+  const decreaseCounter = () => {
+    counter.value--;
+  }
+</script>
+
+<style>
+  .home {
+    text-align: center;
+    padding: 20px;
+  }
+
+  .btn, .counter {
+    font-size: 40px;
+    margin: 10px;  
+  }
+</style>
